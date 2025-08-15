7df7e1220e20291368d75f50244e95a8b41a6e65
marymoreira23 <148573331+marymoreira23@users.noreply.github.com>
2025-08-15 08:42:48 -0300

    ok

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "",
+        enunciado: "porque é essesncial o uso da inteligencia artificial no campo da saude",
         alternativas: [
             {
-                texto: "Utilizando palestras para prevenir o cyberbullying nas escolas.",
-                afirmacao: "Você é uma pessoa reflexiva e busca aprendizado e conhecimento."
+                texto: "Diagnóstico precoce de doenças por meio da análise de imagens médicas.",
+                 afirmacao: "Você é uma pessoa observadora, pois se preocupa com o bem estar dos seus alunos."
             },
             {
                 texto:  "Prestando atenção aos sinais de cyberbullying nas escolas.",
